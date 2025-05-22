179bc1ce4ecf8b1af15a091c90753ad20c4a5753
AndriyNich <163189518+AndriiNych@users.noreply.github.com>
2025-05-22 16:01:07 +0300

    Initial commit from create-refine-app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,33 +45,11 @@ export default function RootLayout({
                     dataProvider={dataProvider}
                     notificationProvider={useNotificationProvider}
                     authProvider={authProviderClient}
-                    resources={[
-                      {
-                        name: "blog_posts",
-                        list: "/blog-posts",
-                        create: "/blog-posts/create",
-                        edit: "/blog-posts/edit/:id",
-                        show: "/blog-posts/show/:id",
-                        meta: {
-                          canDelete: true,
-                        },
-                      },
-                      {
-                        name: "categories",
-                        list: "/categories",
-                        create: "/categories/create",
-                        edit: "/categories/edit/:id",
-                        show: "/categories/show/:id",
-                        meta: {
-                          canDelete: true,
-                        },
-                      },
-                    ]}
                     options={{
                       syncWithLocation: true,
                       warnWhenUnsavedChanges: true,
                       useNewQueryKeys: true,
-                      projectId: "JuGJHg-6AG2U5-xAsvWz",
+                      projectId: "7jqBBf-BWfH7u-azJBAZ",
                     }}
                   >
                     {children}
